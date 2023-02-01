7b518fb5580fb09bf7faa01be35d8b4cf17fb38b
taykim7 <th_77@naver.com>
2023-02-01 21:50:09 +0900

    배포

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  //transpileDependencies: true,
-  outputDir: './docs',
-  publicPath: 'taylab',
+ // transpileDependencies: true,
+ outputDir: './docs',
+ publicPath: '',
 })
