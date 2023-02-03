cc7b7b4504dd944f5e5b977046ded2249e957838
taykim7 <th_77@naver.com>
2023-02-04 00:59:16 +0900

    feature : 날짜, 무게, 코멘트 관련 기능 개발 중

--- src/components/layout/TContents.vue
+++ src/components/layout/TContents.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
-    <button>오늘</button>
-    <i class="fas fa-trash-alt"></i>
-    <h1>{{ year + '.' + month + '.' + day }}</h1>
-    <button>65</button>
-    <button>70</button>
-    <button>75</button>
-    <h1>70.9</h1>
+    <div>
+      <button @click="adjustDate(0)">오늘</button>
+      <i class="fas fa-trash-alt"></i>
+      <div>
+        <button @click="adjustDate(-1)">←</button>
+        <h1>{{ year + '.' + month + '.' + date + ' ' + day  }}</h1>
+        <button @click="adjustDate(+1)">→</button>
+      </div>
+    </div>
+    <hr>
+    <div>
+      <button @click="adjustWeight(65)">65</button>
+      <button @click="adjustWeight(70)">70</button>
+      <button @click="adjustWeight(75)">75</button>
+      <div>
+        <button @click="adjustWeight(-0.1)">전</button>
+        <h1> {{ myWeight }} </h1>
+        <button @click="adjustWeight(+0.1)">후</button>
+      </div>
+    </div>
+    <hr>
     <button>:)</button>
-    <input type="text">
-    <button>저장</button>
+    <input type="text" v-model="myComment">
+    <button @click="saveMyComment()">저장</button>
     <button>분석</button>
   </div>
 </template>
@@ -19,24 +33,59 @@
 export default {
   data: function(){
     return {
+      // 날짜 관련
+      nowDate: {},
       year: '',
       month: '',
+      date: '',
       day: '',
+      dayList: ['일', '월', '화', '수', '목', '금', '토'],
+      // 무게 관련
+      myWeight: 0,
+      // 코멘트 관련
+      myComment: ''
     }
   },
   created() {
+    this.nowDate = new Date();
     this.init()
   },
   methods: {
     init() {
-      this.getDate()
+      this.initDate()
+      this.initWeight()
     },
-    getDate() {
-      const date = new Date();
-      this.year = date.getFullYear();
-      this.month = ('0' + (date.getMonth() + 1));
-      this.day = ('0' + date.getDate());
-
+    initDate() {
+      this.year = this.nowDate.getFullYear();
+      this.month = ('0' + (this.nowDate.getMonth() + 1)).slice(-2);
+      this.date = ('0' + this.nowDate.getDate()).slice(-2);
+      this.day = this.dayList[this.nowDate.getDay()];
+    },
+    // 날짜 조정
+    adjustDate(i) {
+      if (i == 0) {
+        this.nowDate = new Date();
+      } else {
+        this.nowDate.setDate(this.nowDate.getDate() + i)
+      }
+      this.initDate();
+    },
+    initWeight() {
+      // ※ TODO : 어제 날짜
+      this.myWeight = 70.0;
+    },
+    // 무게 조정
+    adjustWeight(i) {
+      if (i >= 60) {
+        this.myWeight = i
+      } else {
+        this.myWeight = Math.round((this.myWeight += i) * 10)/10
+      }
+    },
+    // 코멘트
+    saveMyComment() {
+      // ※ TODO : 해당 내용대로 업데이트
+      alert(this.year + '' + this.month + '' + this.date + ' / ' + this.myWeight + ' / ' + this.myComment);
     }
   }
 }
