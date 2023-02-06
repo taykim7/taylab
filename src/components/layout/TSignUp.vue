0c1658317602a9a43ef57b2aa47b9e950423c91d
taykim7 <th_77@naver.com>
2023-02-06 23:18:54 +0900

    그지같은 파이어베이스

--- src/components/layout/TSignUp.vue
+++ src/components/layout/TSignUp.vue
@@ -2,13 +2,11 @@
   <div>
     <input type="text" v-model="email" placeholder="email"><br>
     <input type="password" v-model="password" placeholder="password"><br>
-    <button v-on:click="signUp">가입하기</button>
+    <button v-on:click="signUp">가입하기ㅇㅇ</button>
   </div>
 </template>
 
 <script>
-  import firebase from "firebase/app";
-
   export default {
     data() {
       return {
@@ -18,15 +16,8 @@
   },
   methods: {
     signUp() {
-      console.log(firebase)
-      // firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
-      //   function(user) {
-      //     alert('회원가입 완료!' + user)
-      //   },
-      //   function(err) {
-      //     alert('에러 : ' + err.message)
-      //   }
-      //);
+      console.log('ㅎㅇ')
+
     }
   }
   }
